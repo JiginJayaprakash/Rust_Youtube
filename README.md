f67337739666f5e1a8a38ad4c13e171074c1d9e1
Jigin Jayaprakash <145117767+JiginJayaprakash@users.noreply.github.com>
2023-12-26 12:18:03 +0000

    Layout added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Rust_Youtube
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+
+cargo --version
+
+cargo run
+
+https://github.com/rwf2/Rocket/blob/v0.5/examples/templating/src/hbs.rs
+
+https://rocket.rs/v0.5/guide/responses/#templates
--- hello-rocket/src/main.rs
+++ hello-rocket/src/main.rs
@@ -1,18 +1,67 @@
-#[macro_use] extern crate rocket;
+#![feature(proc_macro_hygiene, decl_macro)]
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[macro_use] extern crate rocket;
+extern crate rocket_dyn_templates;
+
+// use rocket::response::Redirect;
+use rocket_dyn_templates::Template;
+use rocket::response::Redirect;
+
+
+#[derive(serde::Serialize)]
+struct Message {
+    user: &'static str,
+    body: &'static str
 }
+
+#[derive(serde::Serialize)]
+struct BoardContext {
+    current_user: Option<String>,
+    messages: Vec<Message>,
+    parent: &'static str
+}
+
+#[derive(serde::Serialize)]
+struct AboutContext {
+    parent: &'static str
+}
+
 
 #[get("/world")]              // <- route attribute
 fn world() -> &'static str {  // <- request handler
     "hello, world1!"
 }
 
+#[get("/")]
+fn index() -> Redirect {
+    Redirect::to("/user/anonymous")
+}
+
+#[get("/user/<username>")]
+fn board(username: String) -> Template {
+    Template::render("index", &BoardContext {
+        current_user: Some(username),
+        messages: vec![Message{user: "userA", body: "This is the first test message."},
+                        Message{user: "userB", body: "This is the second test message."}],
+        parent: "layout"
+    })
+}
+
+#[get("/about")]
+fn about() -> Template {
+    Template::render("about", &AboutContext {
+        parent: "layout"
+    })
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![index])
-    .mount("/hello", routes![world])
+    .mount("/", routes![index,about,board])
+    .mount("/hello", routes![world, about])
+    .attach(Template::fairing())
 }
+
+// fn main() {
+//     rocket::build().mount("/", routes![index]).launch();
+// }
